28832f63470c743bb09c23f82b6cbdde7368593e
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-01-23 12:26:32 -0300

    Comple Challenge

--- app.js
+++ app.js
@@ -6,21 +6,58 @@ const ctx = canvas.getContext("2d");
 url.oninput = (e) => {
   const value = e.target.value.trim();
   if (value.length <= 5) {
-    return (e.target.value = null);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    e.target.value = null;
+    return null;
   }
 
   fetch(`https://cors-anywhere.herokuapp.com/${value}`)
     .then((res) => res.blob())
     .then((blob) => {
       const img = new Image();
-      img.onload = () => {
-        preview.append(img);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(img, 0, 0, img.width, img.height);
-        const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        let colors = [];
-        console.log(data.data);
-      };
       img.src = URL.createObjectURL(blob);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      img.onload = () => {
+        const scaleWidth = canvas.width / img.width;
+        const scaleHeight = canvas.height / img.height;
+        const scale = Math.min(scaleWidth, scaleHeight);
+        ctx.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+        const palette = colors();
+        console.clear();
+        palette.forEach((color) =>
+          console.log(
+            "%c" + color[0] + " " + color[1],
+            `background: ${color[0]} ; color: #fff`
+          )
+        );
+      };
     });
+};
+
+const colors = () => {
+  const context = canvas.getContext("2d");
+  const imageData = context.getImageData(
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  ).data;
+  const colorCounts = {};
+
+  const toHex = (value) => {
+    const hex = value.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  };
+
+  for (let i = 0; i < imageData.length; i += 4 * 32 * 32) {
+    const red = toHex(imageData[i]);
+    const green = toHex(imageData[i + 1]);
+    const blue = toHex(imageData[i + 2]);
+    const rgba = `#${red}${green}${blue}`;
+    colorCounts[rgba] = (colorCounts[rgba] || 0) + 1;
+  }
+  let colors = Array.from(new Set(Object.keys(colorCounts)));
+  return colors
+    .sort((a, b) => colorCounts[b] - colorCounts[a])
+    .map((hexColor) => [hexColor, colorCounts[hexColor]]);
 };
